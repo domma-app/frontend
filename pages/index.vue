0d6978fc2b1f13109d94b3dfaae1a86c34c5f7e3
zyrridian <rezkyaf246@gmail.com>
2025-04-28 15:02:06 +0700

    feat: add login page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-50">
+    <div class="text-center">
+      <p class="text-gray-500">Redirecting to login...</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const router = useRouter();
+
+// Redirect to login page
+onMounted(() => {
+  router.push("/login");
+});
+</script>
